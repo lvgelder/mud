917e27d7307f58b33b10cca46488daafe0987fb9
Lisa van Gelder <lisa.vangelder@gmail.com>
2015-02-05 12:56:21 -0500

    test actions too

--- test/mud/brain_test.clj
+++ test/mud/brain_test.clj
@@ -12,9 +12,27 @@
       )
 
 (fact "Take exit sets players new room to be from room from the exit"
-      (take-exit  1 "take exit 1" 1) => irrelevant
+      (take-exit 1 "take exit 1" 1) => irrelevant
       (provided
         (models/exits-by-room 1) => [{:id 1 :description "A madeup door" :to_room 42 :from_room 1}]
         (models/set-player-room 1 42) => irrelevant :times 1
+        )
+      )
+
+(fact "Asking to do something it doesnt understand should return that"
+      (action 1 "madeup string of foo" 1) => "I don't know how to do that"
+      )
+
+(fact "Asking for exits should call list-exits"
+      (action 1 "exits" 1) => irrelevant
+      (provided
+        (list-exits 1 "exits" 1) => irrelevant :times 1
+        )
+      )
+
+(fact "Asking for open door should call take exit"
+      (action 1 "open door" 1) => irrelevant
+      (provided
+        (take-exit 1 "open door" 1) => irrelevant :times 1
         )
       )
