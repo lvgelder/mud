7742041cd087229658d600b7987b7caaf0c8b96e
Lisa van Gelder <lisa.vangelder@gmail.com>
2015-03-04 12:09:02 -0500

    create user only if valid

--- src/mud/views.clj
+++ src/mud/views.clj
@@ -91,25 +91,26 @@
     [:div (:flash req)]
     ))
 
+(defn new-user [params]
+  (models/create-player params)
+  (models/create-user params)
+  (let [pl (models/player-by-name (:name params))
+        usr (models/find-by-username (:username params))]
+    (models/initialize-player-room (:id pl) 1)
+    (models/initialize-player-weapon (:id pl))
+    (models/add-user-role (:id usr) 1)
+    (models/add-user-player (:id usr) (:id pl))
+    )
+  (assoc (response/redirect "/login") :flash "New player created successfully! Please login with your new credentials.")
+  )
+
 (defn make-player [params]
   (let [err (valid/valid-user? params)]
     (if (not(empty? err))
       (assoc (response/redirect "/player/new") :flash (assoc err :form-vals {:username (:username params)
-                                                                             :password (:password params)
-                                                                             :name (:name params)}))
-      (assoc (response/redirect "/login") :flash "success")
-      )
-    ))
-  ;(models/create-player params)
-  ;(models/create-user params)
-  ;(let [pl (models/player-by-name (:name params))
-  ;      usr (models/find-by-username (:username params))]
-  ;  (models/initialize-player-room (:id pl) 1)
-  ;  (models/initialize-player-weapon (:id pl))
-  ;  (models/add-user-role (:id usr) 1)
-  ;  (models/add-user-player (:id usr) (:id pl))
-  ;  )
-;(assoc (response/redirect-after-post "/login") :flash "New player created successfully! Please login with your new credentials."))
+                                                                             :password (:password params) :name (:name params)}))
+      (new-user params)
+      )))
 
 (defn player-page [pl room action]
   (base-page
