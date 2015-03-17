7bff1f85cb036f502960feac4cc702f5b784fd73
Lisa van Gelder <lisa.vangelder@gmail.com>
2015-03-17 15:21:02 -0400

    allow users to talk

--- src/mud/chat.clj
+++ src/mud/chat.clj
@@ -1,14 +1,27 @@
 (ns mud.chat
   (:require [mud.messages :as messages]
-            [clojure.string :as str]))
+            [clojure.string :as str]
+            [mud.models :as models]))
+
+(defn send-message [message to-player-id message-to message-from]
+  do
+  (messages/messsage (format "%s says %s" message-from message) to-player-id)
+  (format "You said %s to %s." message message-to)
+  )
 
 (defn say [player-id action room-id]
-  (messages/messsage action 3)
-  )
-
-(defn msg [message player-id]
-  (messages/messsage message player-id)
-  )
+  (let [message-parts (re-find #"say (.*) to (.*)" action)
+        message (nth message-parts 1)
+        to-name (nth message-parts 2)]
+    (if (< (count message-parts) 3)
+      "I don't know how to do that."
+      (let [player-to-talk-to (models/player-by-name (str/trim to-name))
+            current-player (models/player-by-id player-id)]
+        (cond
+          (not player-to-talk-to) (format "I don't know who %s is." to-name)
+          (not (messages/currently-playing (:id player-to-talk-to))) (format "%s is not there to talk to." to-name)
+          :else (send-message message (:id player-to-talk-to) (str/trim to-name) (:name current-player))
+          )))))
 
 (defn list-players [players player-id]
     (let [players-without-me (filter #(not(= (:id %) player-id)) players)
--- test/mud/chat_test.clj
+++ test/mud/chat_test.clj
@@ -26,3 +26,36 @@
       (list-players [pl1 currPlayer] 1) => "bob is in the room with you."
       (provided
         (messages/currently-playing 5) => true))
+
+(fact "say doesnt work if username unknown"
+      (say 1 "say hello to bob" 1) => "I don't know who bob is."
+      (provided
+        (models/player-by-id 1) => {:id 1 :name "kate"}
+        (models/player-by-name "bob") => nil))
+
+(fact "say doesnt work if you don't say who to say to"
+      (say 1 "say hello" 1) => "I don't know how to do that.")
+
+(fact "say works if user"
+      (say 1 "say hello to bob" 1) => "You said hello to bob."
+      (provided
+        (models/player-by-id 1) => {:id 1 :name "kate"}
+        (models/player-by-name "bob") => {:id 5 :username "bob"}
+        (messages/currently-playing 5) => true
+        (messages/messsage "kate says hello" 5) => irrelevant :times 1 ))
+
+(fact "say works for player name with odd spacing"
+      (say 1 "say hello to   bob the builder " 1) => "You said hello to bob the builder."
+      (provided
+        (models/player-by-id 1) => {:id 1 :name "kate"}
+        (models/player-by-name "bob the builder") => {:id 5 :username "bob the builder"}
+        (messages/currently-playing 5) => true
+        (messages/messsage "kate says hello" 5) => irrelevant :times 1 ))
+
+(fact "you can't say if player not currently playing."
+      (say 1 "say hello to bob" 1) => "bob is not there to talk to."
+      (provided
+        (models/player-by-id 1) => {:id 1 :name "kate"}
+        (models/player-by-name "bob") => {:id 5 :username "bob"}
+        (messages/currently-playing 5) => false
+        (messages/messsage "kate says hello" 5) => irrelevant :times 0 ))
